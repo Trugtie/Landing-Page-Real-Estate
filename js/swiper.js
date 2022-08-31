8b2b1ea833fe8affcb6d4653b551f84a072e0c74
Trugtie <nhamtrungtien2013@gmail.com>
2022-08-31 21:13:00 +0700

    Finish static base

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
